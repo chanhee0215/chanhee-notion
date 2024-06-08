32a326773ec0a3ac30bccad92f1a06b05020c090
chanhee0215 <chanhee015@g.kmou.ac.kr>
2024-06-08 18:07:25 +0900

    노션 페이

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1e5353d5c6b844cd9d8f40222747b5c2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
